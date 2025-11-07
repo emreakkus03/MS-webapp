394101a3e8390d91d3cb288bd29d8e01ca6af2d6
emreakkus03 <emreakkus003@gmail.com>
2025-11-07 15:23:05 +0100

    error fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,9 +9,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY || 'localkey',
-    wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1',
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT || 6001,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 6001,
     forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
